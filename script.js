762b0ddf43cef9654e190a460905d962d1ac073c
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-05-11 13:30:24 +0200

    smtpjs test

--- script.js
+++ script.js
@@ -496,13 +496,32 @@ const validateForm = () =>{
         errorMessage[3].innerText = ""
     })
 
+    // const name = document.getElementById('name').value,
+    // email = document.getElementById('email').value,
+    // subject = document.getElementById('subject').value,
+    // message = document.getElementById('message').value
 
+    function sendEmail() {
+      Email.send({
+      Host: "smtp.elasticemail.com",
+      Username : `${emailInput.value}`,
+      Password : "58279222343A3F9D27AB6D4A0A4FE9EA47D1",
+      To : 'czajnikelektryczny0225@gmail.com',
+      From : `${emailInput.value}`,
+      Subject : `${subjectInput.value}`,
+      Body : `${messageInput.value}`,
+      }).then(
+        message => console.log(`name: ${nameInput.value}, email: ${emailInput.value}, subject: ${subjectInput.value}, message: ${messageInput.value}`)
+      );
+    }
 
     if(!errorFlag){
         submit.classList.add('pending')
         setTimeout(() =>{
             submit.classList.remove('pending') 
             submit.classList.add('success')
+            sendEmail()
+
 
             setTimeout(() =>{
                 submit.classList.remove('success')
