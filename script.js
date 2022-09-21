29adcd458c1bd9d3130e2f5332163cd57cc0e03e
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-09-21 15:24:53 +0200

    fixed accessibility and loading themes before html

--- script.js
+++ script.js
@@ -113,7 +113,6 @@ const typeWriter = () => {
 };
 
 ///////////////  Theme Switcher //////////////////
-let root = document.documentElement;
 const themes = `[
 {
   "id": "dark",
@@ -354,25 +353,25 @@ const themes = `[
 ]`;
 const obj = JSON.parse(themes);
 
-root.style.setProperty("--primary", localStorage.getItem("primary")); // set property values to that saved in local storage
-root.style.setProperty("--secondary", localStorage.getItem("secondary"));
-root.style.setProperty("--elem-color", localStorage.getItem("elem-color"));
-root.style.setProperty("--navbar-color", localStorage.getItem("navbar-color"));
-root.style.setProperty("--accent-color", localStorage.getItem("accent-color"));
-root.style.setProperty("--text", localStorage.getItem("text"));
-root.style.setProperty("--shadow-color", localStorage.getItem("shadow-color"));
-root.style.setProperty(
-  "--scrollbar-color",
-  localStorage.getItem("scrollbar-color")
-);
-root.style.setProperty(
-  "--scrollbar-color-hover",
-  localStorage.getItem("scrollbar-color-hover")
-);
-root.style.setProperty(
-  "--scrollbar-track-color",
-  localStorage.getItem("scrollbar-track-color")
-);
+// root.style.setProperty("--primary", localStorage.getItem("primary")); // set property values to that saved in local storage
+// root.style.setProperty("--secondary", localStorage.getItem("secondary"));
+// root.style.setProperty("--elem-color", localStorage.getItem("elem-color"));
+// root.style.setProperty("--navbar-color", localStorage.getItem("navbar-color"));
+// root.style.setProperty("--accent-color", localStorage.getItem("accent-color"));
+// root.style.setProperty("--text", localStorage.getItem("text"));
+// root.style.setProperty("--shadow-color", localStorage.getItem("shadow-color"));
+// root.style.setProperty(
+//   "--scrollbar-color",
+//   localStorage.getItem("scrollbar-color")
+// );
+// root.style.setProperty(
+//   "--scrollbar-color-hover",
+//   localStorage.getItem("scrollbar-color-hover")
+// );
+// root.style.setProperty(
+//   "--scrollbar-track-color",
+//   localStorage.getItem("scrollbar-track-color")
+// );
 
 const setTheme = (theme) => {
   obj.filter(function (val) {
